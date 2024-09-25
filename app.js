fc2235f95e994d3fc7077d300083f3db199ebed3
yamamon_hayaton <a201136@a.tsukuba-tech.ac.jp>
2024-09-25 15:26:16 +0900

    [add] my introduction

--- app.js
+++ app.js
@@ -13,18 +13,18 @@ const html = `
 <!DOCTYPE html>
 <html>
   <head>
-    <title>こんにちは世界，ここはあたらしい世界だよ，これたかな？</title>
+    <title>hayamamoの遊び場</title>
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
+    // <script>
+    //   setTimeout(() => {
+    //     confetti({
+    //       particleCount: 100,
+    //       spread: 70,
+    //       origin: { y: 0.6 },
+    //       disableForReducedMotion: true
+    //     });
+    //   }, 500);
+    // </script>
     <style>
       @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
       @font-face {
@@ -54,8 +54,9 @@ const html = `
   </head>
   <body>
     <section>
-      ここはあたらしい世界です
+      hayamamo(メンター)
     </section>
+	<p>初めてのrender</p>
   </body>
 </html>
 `
